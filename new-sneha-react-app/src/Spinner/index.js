6eb346d230c9ec2768ce1623be0db5affb99e934
sneha <sneha391998@gmail.com>
2020-09-16 14:26:31 +0530

    Update index.js

--- new-sneha-react-app/src/Spinner/index.js
+++ new-sneha-react-app/src/Spinner/index.js
@@ -1,36 +1,23 @@
-import React, {Component} from 'react';
+import React, {Component, useState} from 'react';
 
-class Spinner extends Component {
-    state = {
-        delta : 0
-    }
-
-    onUpClick = () => {
-        const {delta} = this.state; //when you're getting values from the state..get it with the curly braces
-        const up_action = this.props.up(delta);
-        this.props.dispatch(up_action);
-    };
-
-    onDownClick = () => {
-        const {delta} = this.state;
-        const down_action = this.props.down(delta);
-        this.props.dispatch(down_action);
-    };
-   
-    render() {
-        const { value } = this.props;
-        return (
-            <div>
-                <h3>Spinner</h3>
-                <hr />
-                <label>Delta: </label><br/>
-                <input type = 'number' onChange = { (evt) => this.setState({ delta : parseInt(evt.target.value) || 0 }) } /><br />
-                <input type = "button" value = "down" onClick = {this.onDownClick}/>
-                <span> [ {value} ] </span>
-                <input type = 'button' value = 'up' onClick = {this.onUpClick}/>
-            </div>
-        );
-    };
+const Spinner = ({value, dispatch, up, down}) => {
+    // const {value, dispatch, up, down} = props;
+    const [delta, setDelta] = useState(0);
+    return (
+        <div>
+            <h3>Spinner</h3>
+            <hr />
+            <label>Delta: </label><br/>
+            <input type = 'number' 
+            onChange = { (evt) => 
+                setDelta(parseInt(evt.target.value) || 0 ) 
+                } />
+            <br />
+            <input type = "button" value = "down" onClick = {() => {  dispatch(up(delta)) }}/>
+            <span> [ {value} ] </span>
+            <input type = 'button' value = 'up' onClick = {() => { dispatch(down(delta))} }/>
+        </div>
+    );
 };
 
 export default Spinner;
